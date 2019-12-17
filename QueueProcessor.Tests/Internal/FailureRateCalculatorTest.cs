121638e1183fd05c2f01478170a46e8269d54bb3
Loránd Biró <lorandbiro@outlook.com>
2019-12-17 12:31:02 +0100

    Add FailureRateCalculator with tests.

---
+++ QueueProcessor.Tests/Internal/FailureRateCalculatorTest.cs
@@ -0,0 +1,47 @@
+﻿using QueueProcessor.Mocks;
+using System;
+using Xunit;
+
+namespace QueueProcessor.Internal
+{
+    public class FailureRateCalculatorTest
+    {
+        [Fact]
+        public void CompexScenario()
+        {
+            ClockStub clock = new ClockStub();
+            FailureRateCalculator calculator = new FailureRateCalculator(4, TimeSpan.FromSeconds(1.0), clock);
+
+            void Step(double timestamp, Action<FailureRateCalculator> update, double expectedFailureRate)
+            {
+                clock.SetSeconds(timestamp);
+                update(calculator);
+                Assert.Equal(expectedFailureRate, calculator.GetFailureRate(), 4);
+            }
+
+            Step(0.0, x => x.OnSuccess(), 0.0);
+            Step(1.0, x => x.OnSuccess(), 0.0);
+            Step(2.0, x => x.OnSuccess(), 0.0);
+            Step(3.0, x => x.OnSuccess(), 0.0);
+            Step(4.0, x => x.OnFailure(), 0.25);
+            Step(5.0, x => x.OnFailure(), 0.5);
+            Step(6.0, x => x.OnFailure(), 0.75);
+            Step(7.0, x => x.OnFailure(), 1.0);
+        }
+
+        [Fact]
+        public void ForgetsAfterWindowDuration()
+        {
+            ClockStub clock = new ClockStub();
+            FailureRateCalculator calculator = new FailureRateCalculator(4, TimeSpan.FromSeconds(1.0), clock);
+
+            calculator.OnFailure();
+            calculator.OnSuccess();
+            clock.SetSeconds(3.9);
+            Assert.Equal(0.5, calculator.GetFailureRate(), 4);
+
+            clock.SetSeconds(4.0);
+            Assert.Equal(0.0, calculator.GetFailureRate(), 4);
+        }
+    }
+}
---
+++ QueueProcessor/Internal/FailureRateCalculator.cs
@@ -0,0 +1,97 @@
+﻿using System;
+
+namespace QueueProcessor.Internal
+{
+    public class FailureRateCalculator
+    {
+        private readonly IClock clock;
+        private readonly Bucket[] buckets;
+        private readonly TimeSpan bucketDuration;
+        private int currentBucketIndex;
+        private DateTime currentBucketTimestamp;
+
+        public FailureRateCalculator(int bucketCount, TimeSpan bucketDuration)
+            : this(bucketCount, bucketDuration, new Clock()) { }
+
+        public FailureRateCalculator(int bucketCount, TimeSpan bucketDuration, IClock clock)
+        {
+            if (bucketCount <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(bucketCount), bucketCount, "Bucket count must be larger than zero.");
+            }
+
+            if (bucketDuration <= TimeSpan.Zero)
+            {
+                throw new ArgumentOutOfRangeException(nameof(bucketDuration), bucketDuration, "Bucket duration must be larger than zero.");
+            }
+
+            this.clock = clock ?? throw new ArgumentNullException(nameof(clock));
+
+            this.buckets = new Bucket[bucketCount];
+            this.bucketDuration = bucketDuration;
+
+            this.currentBucketTimestamp = this.clock.Now;
+        }
+
+        public void OnSuccess()
+        {
+            lock (this.buckets)
+            {
+                this.AdvanceBuckets();
+                this.buckets[this.currentBucketIndex].SuccessCount++;
+            }
+        }
+
+        public void OnFailure()
+        {
+            lock (this.buckets)
+            {
+                this.AdvanceBuckets();
+                this.buckets[this.currentBucketIndex].FailureCount++;
+            }
+        }
+
+        public double GetFailureRate()
+        {
+            lock (this.buckets)
+            {
+                this.AdvanceBuckets();
+                int successCount = 0;
+                int failureCount = 0;
+                for (int i = 0; i < this.buckets.Length; i++)
+                {
+                    successCount += this.buckets[i].SuccessCount;
+                    failureCount += this.buckets[i].FailureCount;
+                }
+
+                int sum = successCount + failureCount;
+                return sum == 0 ? 0.0 : failureCount / (double)sum;
+            }
+        }
+
+        private void AdvanceBuckets()
+        {
+            DateTime now = this.clock.Now;
+            TimeSpan diff = now - this.currentBucketTimestamp;
+            while (diff >= this.bucketDuration)
+            {
+                this.currentBucketIndex = (this.currentBucketIndex + 1) % this.buckets.Length;
+                this.currentBucketTimestamp += this.bucketDuration;
+                this.buckets[this.currentBucketIndex].Reset();
+                diff -= this.bucketDuration;
+            }
+        }
+
+        private struct Bucket
+        {
+            public int SuccessCount { get; set; }
+            public int FailureCount { get; set; }
+
+            public void Reset()
+            {
+                this.SuccessCount = 0;
+                this.FailureCount = 0;
+            }
+        }
+    }
+}
